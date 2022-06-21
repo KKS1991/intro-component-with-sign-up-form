3bae78321d5d6f4de8a82c6ed3c55638c9b6a9d9
Kevin Schupp <kevinschupp@outlook.de>
2022-06-21 21:53:22 +0200

    first version - js doesn't work

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const form = document.querySelector("form");
+const fname = document.querySelector("fname");
+const lname = document.querySelector("lname");
+const eaddress = document.querySelector("eaddress");
+const password = document.querySelector("password");
+const btnclaim = document.querySelector("btn-claim");
+
+const errFname = document.getElementById("fname");
+const errLname = document.getElementById("lname");
+const errEmail = document.getElementById("eaddress");
+const errPassword = document.getElementById("password");
+
+function valFname() {
+    if (fname.value === "") {
+        errFname.style.display = "block";
+        fname.style.backgroundImage = 'url("images/icon-error.svg")';
+    }
+}
+
+function valLname() {
+    if (lname.value === "") {
+        errLname.style.display = "block";
+        lname.style.backgroundImage = 'url("images/icon-error.svg")';
+    }
+}
+
+function valEmail() {
+    if (eaddress.value === "") {
+        errEmail.style.display = "block";
+        eaddress.style.backgroundImage = 'url("images/icon-error.svg")';
+    }
+}
+
+function valPassword() {
+    if (password.value === "") {
+        errPassword.style.display = "block";
+        password.style.backgroundImage = 'url("images/icon-error.svg")';
+    }
+}
+
+function valInput () {
+    valFname();
+    valLname();
+    valEmail();
+    valPassword();
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    valInput();
+});
